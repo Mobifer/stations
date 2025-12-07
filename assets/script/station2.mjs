23d6b073e4f510102f5370e8cda86b1cc3f9f89e
illavoluntas <174935274+illavoluntas@users.noreply.github.com>
2025-12-07 13:23:34 +0100

    Log the number of stations created
    Added a console log to display the number of stations.

--- assets/script/station2.mjs
+++ assets/script/station2.mjs
@@ -8,6 +8,8 @@ const template = fs.readFileSync("templates/station.ejs", "utf-8");
 
 fs.mkdirSync("stations", { recursive: true });
 
+console.log("Nombre de stations :", Object.keys(stations).length);
+
 for (const [id, s] of Object.entries(stations)) {
     const html = ejs.render(template, { station: s, id, genererLignesHTML });
     fs.writeFileSync('stations/' + id + '.html', html);
